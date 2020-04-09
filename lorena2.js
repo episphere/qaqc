c0209ee878c2d4fff660b33cf953dfbf31b08ec3
Sandoval <sandovall2@nih.gov>
2020-04-09 12:16:21 -0400

    Update lorena2.js
    Added filter function (cases!=0)

--- lorena2.js
+++ lorena2.js
@@ -20,20 +20,44 @@ for (let [key,value] of Object.entries(qaqc.data)){
 // printprops(qaqc.data)
 
 
-function add(obj){
-  var eth = []
-  for(var col in obj){
-    if( col==="EthnicityClass")
-    //  eth=obj[col]
-      //eth= [...new Set(eth)]
-    //  h +=`<p style= "color:green">${col+ ": " +eth ": n "+ "\n"}</p>`
-    h +=`<p style= "color:green">${col+ ": " +obj[col] + "\n"}</p>`
-    //eth.append(column[row])
-    //console.log(obj[col])//append sort
-}
-}
-add(qaqc.data)
+// function add(obj){
+//   var eth = []
+//   for(var col in obj){
+//     if( col==="EthnicityClass")
+//     //  eth=obj[col]
+//       //eth= [...new Set(eth)]
+//     //  h +=`<p style= "color:green">${col+ ": " +eth ": n "+ "\n"}</p>`
+//     h +=`<p style= "color:green">${col+ ": " +obj[col] + "\n"}</p>`
+//     //eth.append(column[row])
+//     //console.log(obj[col])//append sort
+// }
+// }
+// add(qaqc.data)
 
+//////////////////////////////////////////////////////
+
+//https://www.freecodecamp.org/forum/t/filtering-json-object/244160/3
+let data1 = qaqc.dataArray
+
+const filterCombiner = (d, filterArray) => {
+  for (let fn of filterArray) {
+    if (!fn(d)) {
+      return false;
+    }
+  }
+  return true;
+}
+const filterArray3 = [
+  d => d.status != 0,
+  d => d.ageInt < 40,
+]
+const arr3 = data1.filter(d => filterCombiner(d, filterArray3))
+console.log('arr3', arr3)
+let total = Object.keys(arr3).pop()
+let total1 = Number(total)+1
+h +=`<p>Cases under 40: ${total1}<p/>`
+
+///////////////////////////////////////////////////////////////////
         h += '</p>'
         h += `<p>Save summary statistics below.<p/>`
         h += qaqc.saveFile(qaqc.csvJSON(qaqc.dataTxt))
@@ -61,24 +85,51 @@ let dat = qaqc.data
   //       dat[inArray[i]].forEach(function(val, index)
   //                               {if (inArray[i]=="status"){if (val===0){console.log("case")}}
   //                               })}
-https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements?noredirect=1&lq=1
-var arr = qaqc.data["status"]
-
-var counts = {};
+//https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements?noredirect=1&lq=1
+// function count_status(obj, variable,name1, num1, name2,  num2, name3, num3){
+// var arr = obj[variable]
+// var counts = {};
  
-for (var i = 0; i < arr.length; i++) {
-  var num = arr[i];
-  counts[num] = counts[num] ? counts[num] + 1 : 1;
- }
-console.log(counts)
-let emptArr=[]
+// for (var i = 0; i < arr.length; i++) {
+//   var num = arr[i];
+//   counts[num] = counts[num] ? counts[num] + 1 : 1;
+//  }
+// console.log(counts)
+// let emptArr=[]
+// emptArr.push(counts)
 
-emptArr.push(counts)
+// for (let [key,value] of Object.entries(counts)){console.log(`${key}: ${value}`);}
+// counts[name1]=counts[num1]
+// delete counts[num1]
+// counts[name2]=counts[num2]
+// delete counts[num2]
+// counts[name3]=counts[num3]
+// delete counts[num3]
+// console.log(counts)
+// }
+// count_status(qaqc.data, "status", "case",1,"control", 0, "case2", 2)
+////////////////////////////////////////////////////////////////////////////////////////
 
-for (let [key,value] of Object.entries(counts)){console.log(`${key}: ${value}`);}
-counts["case"]=counts[1]
-delete counts[1]
-console.log(counts)
+
+/////////////////////////
+// https://stackoverflow.com/questions/24444738/sum-similar-keys-in-an-array-of-objects
+// var holder = {};
+
+// obj.forEach(function(d) {
+//   if (holder.hasOwnProperty(d.name)) {
+//     holder[d.name] = holder[d.name] + d.value;
+//   } else {
+//     holder[d.name] = d.value;
+//   }
+// });
+
+// var obj2 = [];
+
+// for (var prop in holder) {
+//   obj2.push({ name: prop, value: holder[prop] });
+// }
+
+// console.log(obj2);
  return h
     }
 // }
